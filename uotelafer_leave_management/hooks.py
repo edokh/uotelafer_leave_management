91bfa049a5c87ed897b229dde1c7dbd42f85374a
edokh <eido.khudyda@gmail.com>
2026-04-28 13:07:58 +0300

    Add translation fixtures

--- uotelafer_leave_management/hooks.py
+++ uotelafer_leave_management/hooks.py
@@ -247,3 +247,11 @@ ppermission_query_conditions = {
 # List of apps whose translatable strings should be excluded from this app's translations.
 # ignore_translatable_strings_from = []
 
+fixtures = [
+    {
+        "dt": "Translation",
+        "filters": [["language", "in", ["ar", "en"]]]
+    }
+]
+
+
